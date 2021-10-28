ee9bcdbefb1ebd6c895f2c0e9fe0fee46143cb07
hojuly <hojune.choi.me@gmail.com>
2021-10-28 09:47:00 +0900

    CH5 L5 add advanced function

--- MyPlaygroundFunc.playground/Contents.swift
+++ MyPlaygroundFunc.playground/Contents.swift
@@ -60,3 +60,50 @@ func printFullName3(_ last: String, _ first: String) -> String {
 }
 let name = printFullName3("Choi", "Hojune")
 name
+
+// advanced func
+
+// overload
+func printTotalPrice(price: Int, count: Int) {
+    print("Total Price: \(price * count)")
+}
+
+func printTotalPrice(price: Double, count: Double) {
+    print("Total Price: \(price * count)")
+}
+
+// In-out parameter
+
+// Bad
+//func incrementAndPrint(_ value: Int) {
+//    value += 1 --> swift에서 불가능
+//    print(value)
+//}
+// Good
+var value = 3
+func incrementAndPrint(_ value: inout Int) {
+    value += 1
+    print(value)
+}
+incrementAndPrint(&value)
+
+// parm = func
+func add(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+func subtract(_ a: Int, _ b: Int) -> Int {
+    return a - b
+}
+
+var function = add
+function(4, 2)
+function = subtract
+function(4, 2)
+
+func printResult(_ function: (Int, Int) -> Int, _ a: Int, _ b: Int){ //함수 타입 같아야 함!
+    let result = function(a, b)
+    print(result)
+}
+printResult(add, 10, 5)
+printResult(subtract, 10, 5)
